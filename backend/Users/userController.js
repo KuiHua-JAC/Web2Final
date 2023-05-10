4be855a050fb7d0c945fd6fca54d5f3bb5a47dfa
AymericBriereJAC <98484632+AymericBriereJAC@users.noreply.github.com>
2023-05-10 01:50:02 -0400

    added my backend and converted as much as possible t o work right away

---
+++ backend/Users/userController.js
@@ -0,0 +1,288 @@
+////Aymeric Code
+
+
+const express = require("express");
+const router = express.Router();
+const routeRoot = "/";
+const userModel = require("./userModel.js");
+const logger = require("../logger.js");
+const { DatabaseError } = require("../error/DatabaseError.js");
+const { InvalidInputError } = require("../error/InvalidInputError.js");
+
+/**
+
+Handles HTTP POST requests to the '/user' endpoint to add a new user to the database.
+@param {Object} request - The HTTP request object.
+@param {Object} response - The HTTP response object.
+*/
+router.post("/user", handleHttpNewRequest);
+async function handleHttpNewRequest(request, response) {
+try {
+const { email, password, firstName, lastName, userName, isAdmin } = request.body;
+let newUser = await userModel.addUser(email, password, firstName, lastName, userName, isAdmin);
+response.status(200);
+response.send(newUser);
+} catch (err) {
+logger.error("Adding a new user failed: " + err);
+if (err instanceof DatabaseError) {
+response.status(500);
+response.send({
+errorMessage: "Adding a user failed." + err.message,
+});
+} else if (err instanceof InvalidInputError) {
+response.status(400);
+response.send({
+errorMessage: "Adding a user failed." + err.message,
+});
+} else {
+response.status(500);
+response.send({
+errorMessage:
+"Adding a user failed. Unexpected error occurred" + err.message,
+});
+}
+}
+}
+/**
+
+Handles HTTP GET requests to the '/show' endpoint to find a single user in the database.
+@param {Object} request - The HTTP request object.
+@param {Object} response - The HTTP response object.
+*/
+router.get("/show/:username", handleHttpShowRequest);
+async function handleHttpShowRequest(request, response) {
+try {
+let foundUser = await userModel.getSingleUser(request.params.username);
+response.status(200);
+response.send(foundUser);
+} catch (err) {
+logger.error("Getting a user failed: " + err);
+if (err instanceof DatabaseError) {
+response.status(500);
+response.send({
+errorMessage: "Getting a user failed." + err.message,
+});
+} else if (err instanceof InvalidInputError) {
+response.status(400);
+response.send({ errorMessage: "Getting a user failed." + err.message });
+} else {
+response.status(500);
+response.send({
+errorMessage: "Getting a user failed. (Unexpected)" + err.message,
+});
+}
+}
+}
+/**
+
+Handles HTTP GET requests to the '/showAll' endpoint to find all users in the database.
+@param {Object} request - The HTTP request object.
+@param {Object} response - The HTTP response object.
+*/
+router.get("/showAll", handleHttpShowAllRequest);
+async function handleHttpShowAllRequest(request, response) {
+try {
+let userArray = await userModel.getAllUsers();
+response.status(200);
+response.send(userArray);
+} catch (err) {
+logger.error("Get all users failed: " + err);
+if (err instanceof DatabaseError) {
+response.status(500);
+response.send({
+errorMessage: "Getting all users failed." + err.message,
+});
+} else if (err instanceof InvalidInputError) {
+response.status(400);
+response.send({
+errorMessage: "Getting all users failed." + err.message,
+});
+} else {
+response.status(500);
+response.send({
+errorMessage:
+"Getting all users failed. Unexpected error occurred" + err.message,
+});
+}
+}
+}
+/**
+
+Handles HTTP DELETE requests to the '/delete' endpoint to delete a single user from the database.
+
+@param {Object} request - The HTTP request object.
+@param {Object} response - The HTTP response object.
+*/
+router.delete("/delete/:username", handleHttpDeleteRequest);
+async function handleHttpDeleteRequest(request, response) {
+try {
+if (await userModel.deleteSingleUser(request.params.username)) {
+response.status(200);
+response.send({ message: "The user has been successfully deleted" });
+} else {
+response.status(500);
+response.send({
+errorMessage: "Deleting user failed: " + err.message,
+});
+}
+} catch (err) {
+logger.error("Deleting user failed: " + err);
+if (err instanceof DatabaseError) {
+response.status(500);
+response.send({
+errorMessage: "Deleting user failed: " + err.message,
+});
+} else if (err instanceof InvalidInputError) {
+response.status(400);
+response.send({
+errorMessage: "Deleting user failed: " + err.message,
+});
+} else {
+response.status(500);
+response.send({
+errorMessage:
+"Deleting user failed. Unexpected error occurred " + err.message,
+});
+}
+}
+}
+
+/**
+
+Handles HTTP PUT requests to the '/updateName' endpoint to update the name of a single user in the database.
+@param {Object} request - The HTTP request object.
+@param {Object} response - The HTTP response object.
+*/
+router.put("/updateName/:username", handleHttpUpdateNameRequest);
+async function handleHttpUpdateNameRequest(request, response) {
+const { newName: newUsername } = request.body;
+try {
+if (await userModel.updateUserName(request.params.userName, newUsername)) {
+response.status(200);
+response.send({
+message: "The user's name has been successfully updated",
+});
+} else {
+response.status(500);
+response.send({
+errorMessage: "Updating user name failed: " + err.message,
+});
+}
+} catch (err) {
+logger.error("Update user name failed: " + err);
+if (err instanceof DatabaseError) {
+response.status(500);
+response.send({
+errorMessage: "Update user name failed: " + err.message,
+});
+} else if (err instanceof InvalidInputError) {
+response.status(400);
+response.send({
+errorMessage: "Update user name failed: " + err.message,
+});
+} else {
+response.status(500);
+response.send({
+errorMessage:
+"Update user name failed. Unexpected error occurred " + err.message,
+});
+}
+}
+}
+/**
+
+Handles HTTP PUT requests to the '/updateEmail' endpoint to update the email of a single user in the database.
+@param {Object} request - The HTTP request object.
+@param {Object} response - The HTTP response object.
+*/
+router.put("/updateEmail/:username", handleHttpUpdateEmailRequest);
+async function handleHttpUpdateEmailRequest(request, response) {
+const { newEmail } = request.body;
+try {
+if (await userModel.updateUserEmail(request.params.username, newEmail)) {
+response.status(200);
+response.send({
+message: "The user's email has been successfully updated",
+});
+} else {
+response.status(500);
+response.send({
+errorMessage: "Updating user email failed: " + err.message,
+});
+}
+} catch (err) {
+logger.error("Update user email failed: " + err);
+if (      err instanceof DatabaseError) {
+  response.status(500);
+  response.send({
+    errorMessage: "Update user email failed: " + err.message,
+  });
+} else if (err instanceof InvalidInputError) {
+  response.status(400);
+  response.send({
+    errorMessage: "Update user email failed: " + err.message,
+  });
+} else {
+  response.status(500);
+  response.send({
+    errorMessage:
+      "Update user email failed. Unexpected error occurred " + err.message,
+  });
+}
+}
+}
+
+//TODO add secrutity and ask to provide current password and check if it is valid
+/**
+* Handles HTTP PUT requests to the '/updatePassword' endpoint to update the password of a single user in the database.
+* @param {Object} request - The HTTP request object.
+* @param {Object} response - The HTTP response object.
+*/
+router.put("/updatePassword/:username", handleHttpUpdatePasswordRequest);
+async function handleHttpUpdatePasswordRequest(request, response) {
+const { newPassword } = request.body;
+try {
+if (await userModel.updateUserPassword(request.params.username, newPassword)) {
+response.status(200);
+response.send({
+  message: "The user's password has been successfully updated",
+});
+} else {
+response.status(500);
+response.send({
+  errorMessage: "Updating user password failed: " + err.message,
+});
+}
+} catch (err) {
+logger.error("Update user password failed: " + err);
+if (err instanceof DatabaseError) {
+response.status(500);
+response.send({
+  errorMessage: "Update user password failed: " + err.message,
+});
+} else if (err instanceof InvalidInputError) {
+response.status(400);
+response.send({
+  errorMessage: "Update user password failed: " + err.message,
+});
+} else {
+response.status(500);
+response.send({
+  errorMessage:
+    "Update user password failed. Unexpected error occurred " + err.message,
+});
+}
+}
+}
+
+module.exports = {
+handleHttpNewRequest,
+handleHttpDeleteRequest,
+handleHttpShowAllRequest,
+handleHttpShowRequest,
+handleHttpUpdateNameRequest,
+handleHttpUpdateEmailRequest,
+handleHttpUpdatePasswordRequest,
+router,
+routeRoot,
+};
---
+++ backend/Users/userModel.js
@@ -0,0 +1,215 @@
+///Aymeric Code
+
+const { getUserCollection } = require("../dbConnection.js");
+const { DatabaseError } = require("../error/DatabaseError.js");
+const { InvalidInputError } = require("../error/InvalidInputError.js");
+const validateUtils = require("./validateUtils.js");
+const validator = require("validator");
+const logger = require("../logger.js");
+
+/**
+ * Adds a new document to the collection, which is a user that contains an email, password, first name, last name, username and isAdmin.
+ * @param {string} email is the email of the user
+ * @param {string} password is the password of the user
+ * @param {string} firstName is the first name of the user
+ * @param {string} lastName is the last name of the user
+ * @param {string} username is the username of the user
+ * @param {boolean} isAdmin is the user an admin?
+ * @return the user object if successful, otherwise
+ * @throws {DatabaseError,InvalidInputError} if the document could not be added or if the input is invalid
+ * @throws {InvalidInputError} if there is an invalid input
+ */
+async function addUser(email, password, firstName, lastName, username, isAdmin) {
+  validateUtils.isValid(email, password, firstName, lastName, username, isAdmin)  //no need to throw errors here as the method is already throwing errors if there is bad input
+
+  //Valid username, but we dont want duplicate. Check if already in database. 
+  //Cannot check in the validateUtils.js file as this require a database connection
+  if (!(await userCollection.findOne({ username: username }) == null)) throw new InvalidInputError("The user " + username + " is already in the database");
+
+  const newUser = {
+    email: email,
+    password: password,
+    firstname: firstName,
+    lastname: lastName,
+    username: username,
+    isadmin: isAdmin
+  };
+
+  try {
+    await userCollection.insertOne(newUser)
+
+    return newUser;
+  } catch (error) {
+    throw new DatabaseError("There was an error while inserting the data in the database: " + error.message);
+  }
+}
+
+/**
+ * Retrieves a single user document from the database based on the user's email.
+ * @param {string} email - The email of the user.
+ * @throws {Error} If no user document is found.
+ * @throws {DatabaseError} If there is an error getting the user document.
+ */
+async function getSingleUserByEmail(email) {
+  try {
+    const user = await getUserCollection().findOne({ email: email });
+
+    if (!user) {
+      throw new Error("No user found");
+    }
+    return user;
+  } catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error getting user");
+  }
+}
+
+/**
+ * Gets the info of a given user
+ * 
+ * @param {string} username username of the user to get the info of
+ * @returns the user object
+ */
+async function getSingleUserByUsername(username) {
+  try {
+    let user = await userCollection.findOne({ username: username });
+
+    if (!user) {
+      throw new Error("No user found");
+    }
+    return user;
+  } catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error getting user");
+  }
+}
+
+/**
+ * Gets all users from the database.
+ * @returns {Promise<Array>} - An array of all users in the database.
+ * @throws {DatabaseError} - If an error occurs while getting the users from the database.
+ */
+async function getAllUsers() {
+  try {
+    const users = await getUserCollection().find({}).toArray();
+    if (!users || users.length === 0) {
+      throw new Error("No users found");
+    }
+    return users;
+  } catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error getting users");
+  }
+}
+
+/**
+ * Deletes a single user from the database.
+ * @param {Object} username - The user to be deleted from the database.
+ * @throws {DatabaseError} - If an error occurs while deleting the user from the database.
+ * @returns true if the operation was succefull
+ */
+async function deleteSingleUser(username) {
+  try {
+    const result = await getUserCollection().deleteOne({ userName: username });
+
+    if (result.deletedCount === 0) {
+      throw new Error("No user found with the given username");
+    }
+
+    return true;
+  } catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error deleting user");
+  }
+}
+
+/**
+ * Updates the Username of a single user in the database.
+ * @param {String} oldUsername - Old Username of the user to update.
+ * @param {string} newusername - The new username of the user.
+ * @throws {DatabaseError} - If an error occurs while updating the user in the database.
+ * @returns true if the operation was succefull
+ */
+async function updateUserame(oldUsername, newusername) {
+  validateUtils.isValidUsername(newUsername); //will throw if username is invalid
+
+  //Valid username, but we dont want duplicate. Check if already in database
+  if (!(await userCollection.findOne({ username: newUsername }) == null)) throw new InvalidInputError("The user " + newUsername + " is already in the database");
+
+  try {
+    // Update the user's username
+    const result = await userCollection.updateOne(
+      { username: oldUsername },
+      { $set: { username: newUsername } }
+    );
+    if (result.modifiedCount === 0) {
+      throw new InvalidInputError("User not found or username is the same as the current one");
+    }
+
+    return result.modifiedCount;
+  }
+  catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error updating the user");
+  }
+}
+
+
+/**
+ * Updates the email of a single user in the database.
+ * @param {String} username - The username to be updated in the database.
+ * @param {string} email - The new email of the user.
+ * @throws {DatabaseError} - If an error occurs while updating the user in the database.
+ */
+async function updateUserEmail(username, email) {
+  validateUtils.isValidEmail(email)
+  try {
+    // Update the user's username
+    const result = await userCollection.updateOne(
+      { username: username },
+      { $set: { email: email } }
+    );
+    if (result.modifiedCount === 0) {
+      throw new InvalidInputError("User not found or username is the same as the current one");
+    }
+
+    return result.modifiedCount;
+  } catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error updating the user");
+  }
+}
+
+/**
+ * Updates the password of a single user in the database.
+ * @param {string} username - The user to be updated in the database.
+ * @param {string} password - The new password of the user.
+ * @throws {DatabaseError} - If an error occurs while updating the user in the database.
+ */
+async function updateUserPassword(username, password) {
+  validateUtils.isValidPassword(password);
+
+  try {
+    // Update the user's username
+    const result = await userCollection.updateOne(
+      { username: username },
+      { $set: { password: password } }
+    );
+
+    return true;
+  } catch (err) {
+    logger.fatal(err.message);
+    throw new DatabaseError("Error updating the user");
+  }
+}
+
+module.exports = {
+  addUser,
+  getSingleUserByEmail,
+  getAllUsers,
+  deleteSingleUser,
+  getSingleUserByUsername,
+  updateUserEmail,
+  updateUserPassword,
+  updateUserame
+};
