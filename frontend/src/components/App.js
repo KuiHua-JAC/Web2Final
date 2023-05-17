9149064e0aed28b647b7adb560c0d09ecc469764
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-05-17 08:17:00 -0400

    add necessary files for create and update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,10 +3,13 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Cars from "./pages/Cars";
-import About from "./pages/About";
 import Reviews from "./pages/Reviews";
 import Car from "./pages/Car";
 import Review from "./pages/Review";
+import UpdateCar from "./pages/UpdateCar";
+import AddCar from "./pages/AddCar";
+import AddReview from "./pages/AddReview";
+import UpdateReview from "./pages/UpdateReview";
 
 export default function App() {
   return (
@@ -16,9 +19,12 @@ export default function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/reviews/:title" element={<Review />} />
+        <Route path="/reviews/add" element={<AddReview />} />
+        <Route path="/reviews/update/:title" element={<UpdateReview />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/cars/:make/:model/:year" element={<Car />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/cars/add" element={<AddCar />} />
+        <Route path="/cars/update/:title" element={<UpdateCar />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
--- frontend/src/components/pages/About.js
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div>
-      <p>About page to be incorporated later</p>
-    </div>
-  );
-}
